a60547ec4ff23dad0f3f51c81063ccda4ae96a2f
Hao-Qian <hao_qian_github@yahoo.com>
2016-06-30 00:33:34 +0100

    added eclasticSearch client

---
+++ ElasticSearchClient.js
@@ -0,0 +1,10 @@
+var elasticsearch = require('elasticsearch')
+
+var client = new elasticsearch.Client();
+client.cluster.health(function(err, resp){
+	if(err){
+		console.error(err.message);
+	}else{
+		console.dir(resp);
+	}
+})
